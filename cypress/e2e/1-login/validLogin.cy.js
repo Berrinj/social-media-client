02df54936ec6fa6f5cf7f08aade7661b7403ac00
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-08-15 23:26:28 +0200

    a valid login test has passed cypress test

--- cypress/e2e/1-login/validLogin.cy.js
+++ cypress/e2e/1-login/validLogin.cy.js
@@ -1,10 +1,12 @@
-describe("testing if user can login with valid credentials", () => {
+describe("Noroff login form test", () => {
   it("should login with valid credentials", () => {
     cy.visit("/");
+    cy.wait(500);
     cy.get("button[data-auth='login']").last().click();
-    cy.get("input[name=email]").type("fridafever@stud.noroff.no");
-    cy.get("input[name=password]").type("123456789");
-    cy.get("button[type=submit]").click();
+    cy.wait(500);
+    cy.get("input#loginEmail").type("fridafever@stud.noroff.no");
+    cy.get("input#loginPassword").type("123456789");
+    cy.get(".modal-footer").contains("Login").click();
     cy.get("h4.profile-name").should("contain.text", "Frida");
   });
 });
