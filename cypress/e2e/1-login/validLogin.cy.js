519c39a008d84bf298e15badd256dc0e6829d6a5
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-08-15 22:31:26 +0200

    first error fix on login button click

--- cypress/e2e/1-login/validLogin.cy.js
+++ cypress/e2e/1-login/validLogin.cy.js
@@ -1,7 +1,7 @@
 describe("testing if user can login with valid credentials", () => {
   it("should login with valid credentials", () => {
     cy.visit("/");
-    cy.get("button[data-auth='login']").click();
+    cy.get("button[data-auth='login']").first().click();
     cy.wait(500);
     cy.get("input[name=email]").type("fridafever@stud.noroff.no");
     cy.get("input[name=password]").type("123456789");
