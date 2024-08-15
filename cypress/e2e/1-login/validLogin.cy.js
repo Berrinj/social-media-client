b48bc53ce2478a90cdf42b1ffa77e60fb068ae62
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-08-15 22:43:10 +0200

    targeting the button I want, hopefully

--- cypress/e2e/1-login/validLogin.cy.js
+++ cypress/e2e/1-login/validLogin.cy.js
@@ -1,12 +1,10 @@
 describe("testing if user can login with valid credentials", () => {
   it("should login with valid credentials", () => {
     cy.visit("/");
-    cy.get("button[data-auth='login']").first().click();
-    cy.wait(500);
+    cy.get("form[id=registerform] button[data-auth='login']").click();
     cy.get("input[name=email]").type("fridafever@stud.noroff.no");
     cy.get("input[name=password]").type("123456789");
     cy.get("button[type=submit]").click();
-    cy.wait(500);
     cy.get("h4.profile-name").should("contain.text", "Frida");
   });
 });
