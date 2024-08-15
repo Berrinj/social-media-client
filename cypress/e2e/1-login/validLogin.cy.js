757095310e71202c0accb4563611a0bf9dd007a8
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-08-15 22:50:33 +0200

    trying to target the correct login button

--- cypress/e2e/1-login/validLogin.cy.js
+++ cypress/e2e/1-login/validLogin.cy.js
@@ -1,7 +1,7 @@
 describe("testing if user can login with valid credentials", () => {
   it("should login with valid credentials", () => {
     cy.visit("/");
-    cy.get("form[id=registerform] button[data-auth='login']").click();
+    cy.get("form#registerform] button[data-auth=login]").click();
     cy.get("input[name=email]").type("fridafever@stud.noroff.no");
     cy.get("input[name=password]").type("123456789");
     cy.get("button[type=submit]").click();
